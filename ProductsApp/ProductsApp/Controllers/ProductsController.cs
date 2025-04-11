98a27ad8ac02f1cb5ab173b2893fe77c0a32f723
kfon <tuo64805@temple.edu>
2025-04-11 13:58:17 -0400

    Intentional changes to make tests fail

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Bromato Soup", Category = "Broceries", Price = 3 },
+            new Product { Id = 2, Name = "Bro-Bro", Category = "Broys", Price = 3.33M },
+            new Product { Id = 3, Name = "Brommer", Category = "Broware", Price = 33.99M }
         };
         public ProductsController()
         {
